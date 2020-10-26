597dbcfa7ca9637271ad7100692c5cd57f6f438b
Yiliang Chen <yiliangchen0113@gmail.com>
2020-10-26 16:53:43 +0000

    Automatically backed up by Learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -1,11 +1,13 @@
 require 'pry'
 
 def my_all?(collection)
-  i = 0 
-  while i < collection.length 
-  yield collection[i]
-  i +=  1 
-  end   
+  i = 0
+  block_return_values = []
+  while i < collection.length
+    block_return_values << yield(collection[i])
+    i = i + 1
+  end
+  block_return_values
 end
 
 my_all?([1,2,3]) {|i| i < 2}
